c47d88973f9e886d4cc79253074a39ca9996a930
StAjora <gultembergue.ufs@gmail.com>
2022-07-06 21:48:39 -0300

    Simple Create Form

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -17,5 +17,10 @@ namespace SalesWebMVC.Services
         {
             return _context.Seller.ToList();
         }
+        public void insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
